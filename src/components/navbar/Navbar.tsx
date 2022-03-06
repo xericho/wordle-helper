7d49cd9c9cad93e96731f70cc1edcda6c108138f
Eric Ho <xeric.hox@gmail.com>
2022-03-05 22:14:02 -0800

    centered game title

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,12 +21,12 @@ import {
   }: Props) => {
     return (
       <div className="navbar ">
-        <div className="navbar-content px-5">
+        <div className="navbar-content ">
           <InformationCircleIcon
-            className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
+            className="h-6 w-6 ml-2 cursor-pointer dark:stroke-white"
             onClick={() => setIsInfoModalOpen(true)}
           />
-          <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
+          <p className="text-xl game-title font-bold dark:text-white">{GAME_TITLE}</p>
           <div className="right-icons">
             <SupportIcon
               className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
